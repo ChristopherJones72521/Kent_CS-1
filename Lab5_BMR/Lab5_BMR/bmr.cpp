b8bc8b83b60864d0df853a1529bb953560a8c48c
Christopher Jones <christopherjones72521@gmail.com>
2014-09-27 13:20:31 -0400

    Final Version of Lab5_BMR

--- Lab5_BMR/Lab5_BMR/bmr.cpp
+++ Lab5_BMR/Lab5_BMR/bmr.cpp
@@ -1,9 +1,9 @@
 /* Author: Christopher Jones
- * Date: 9/16/2014
- * Name: Lab5 - Basal Metabolic Rate Study
- * This program calculates the BMR and number of bagels a person can consume in a day
- * CS - 13005
- */
+* Date: 9/16/2014
+* Name: Lab5 - Basal Metabolic Rate Study
+* This program calculates the BMR and number of bagels a person can consume in a day
+* CS - 13005
+*/
 
 #include <cmath> 
 #include <iostream> 
@@ -25,19 +25,24 @@ int main()
 	cin >> feet;
 	cout << "How many additional inches tall are you?" << endl;
 	cin >> inches;
-	cout >> "What is your age (don't worry I won't tell anyone.)"
-		cin >> age;
+	cout << "What is your age (don't worry I won't tell anyone.)" << endl;
+	cin >> age;
 
 	height = (feet * 12) + inches;
 
-	if (gender = "m")
+	if (gender == 'm') {
 		bmrMen = (weight, height, age);
-	result = (66 + (6.3 * weight) + (12.9 * height) - (6.8 * age));
+		result = (66 + (6.3 * weight) + (12.9 * height) - (6.8 * age));
+	}
 	else
+	{
 		bmrWomen = (weight, height, age);
-	result = (655 + (4.3 * weight) + (4.7 * height) - (4.7 * age));
+		result = (655 + (4.3 * weight) + (4.7 * height) - (4.7 * age));
+	}
 
-	cout << result;
+	cout << "Your Basal Metabolic Rate is:" << result << endl;
 
+	cout << "Press enter to exit..." << endl;
+	cin.get();
 	return 0;
 }
